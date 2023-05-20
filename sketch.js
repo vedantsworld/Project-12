2bafce5080e193ffa6db7e4ba046e77080c3d908
vedantsworld <129420728+vedantsworld@users.noreply.github.com>
2023-05-20 12:55:28 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var marks = [35, 38, 42, 45, 43, 34, 46, 41, 48, 32];
+
+for (var i = 0; i < marks.length; i++) {
+  if (marks[i] == 34) {
+    var position = i
+  }
+}
+console.log(position)
+
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(30);
+}
+
+
+
